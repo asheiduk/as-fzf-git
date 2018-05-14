453a494deca63490f2f1313aa9a27ffed4a13a91
Andreas Heiduk <asheiduk@gmail.com>
2018-05-14 15:06:34 +0100

    key-bindings.bash: provide custom function fzf-bind
    Wrapping a custom function makes it easier to change the glue code
    in the near future.

--- key-binding.bash
+++ key-binding.bash
@@ -1,6 +1,14 @@
 bind '"\er": redraw-current-line'
-bind '"\C-g\C-f": "$(fzf-gf)\e\C-e\er"'
-bind '"\C-g\C-b": "$(fzf-gb)\e\C-e\er"'
-bind '"\C-g\C-t": "$(fzf-gt)\e\C-e\er"'
-bind '"\C-g\C-h": "$(fzf-gh)\e\C-e\er"'
-bind '"\C-g\C-r": "$(fzf-gr)\e\C-e\er"'
+
+fzf-bind () {
+	local keyseq="$1"
+	local cmd="$2"
+
+	bind "'$keyseq': '\$($cmd)\e\C-e\er'"
+}
+
+fzf-bind '\C-g\C-f' fzf-gf
+fzf-bind '\C-g\C-t' fzf-gt
+fzf-bind '\C-g\C-b' fzf-gb
+fzf-bind '\C-g\C-h' fzf-gh
+fzf-bind '\C-g\C-r' fzf-gr
